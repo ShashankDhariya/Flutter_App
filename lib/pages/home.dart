0a1c0582c62510e4ef85f5893f6769f742341254
Shashank Dhariya <dhariyashashank@gmail.com>
2022-11-25 01:25:08 +0530

    GridView

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import '../models/catalog.dart';
-import '../widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -39,13 +38,42 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModels.items.isNotEmpty)?
-        ListView.builder(
-          itemCount: CatalogModels.items.length,
-          itemBuilder: (context, index) =>
-            ItemWidget(
-              item: CatalogModels.items[index],
+        GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+
             ),
-        ): const Center(
+          itemBuilder: ((context, index) {
+            final item = CatalogModels.items[index];
+            return Card(
+              // clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: GridTile(
+                header: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    color: Colors.deepOrange,
+                  ),
+                  child: Text(item.name),
+                ),
+                footer: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    color: Colors.yellow,
+                  ),
+                  child: Text("Rs.${item.price.toString()}"),
+                ),
+                child: Image.network(item.image),
+              ),
+            );
+          }),
+          itemCount: CatalogModels.items.length,
+        )
+      : const Center(
             child: CircularProgressIndicator(),
         )
       ),
